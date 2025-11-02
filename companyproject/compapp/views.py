e42321dad6d18dae5f7aa7a3d869a9dc9e7e2853
salhmlan880-lang <salhmlan880@gmail.com>
2025-11-02 14:21:09 +0300

    .

--- companyproject/compapp/views.py
+++ companyproject/compapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import employee  
+
+def employee_list(request):
+    employees = employee.objects.all()  
+    return render(request, 'employee_list.html', {'employees': employees})
