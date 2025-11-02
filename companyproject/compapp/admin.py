e8601d1cdb594d499a35bb47047664789b0a325d
salhmlan880-lang <salhmlan880@gmail.com>
2025-11-02 13:55:25 +0300

    .

--- companyproject/compapp/admin.py
+++ companyproject/compapp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import employee
 
-admin.site.register(employee)
+
 # Register your models here.
+@admin.register(employee)
+
+class employeeAdmin(admin.ModelAdmin):
+    list_display=('name','position','joined_on','phone_number')
+    search_fields = ('name','position')
+    list_filter=('joined_on',)
+    pass
+    
+
