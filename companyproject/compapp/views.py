9b7417e0fb13bb315663ae567667bfa7e4b49930
salhmlan880-lang <salhmlan880@gmail.com>
2025-11-02 14:23:18 +0300

    Update views.py

--- companyproject/compapp/views.py
+++ companyproject/compapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import employee  
 
 def employee_list(request):
-    employees = employee.objects.all()  
+    employees = employee.objects.all().order_by('name')  
     return render(request, 'employee_list.html', {'employees': employees})
